953759158a3dac0d4587aff66f7a2613c256e808
Johnny <jkkall7@gmail.com>
2015-04-27 02:44:33 +0300

    refactor

--- src/com/dreamlock/CommandHandler.java
+++ src/com/dreamlock/CommandHandler.java
@@ -22,44 +22,50 @@ public class CommandHandler {
         JsonArray sentences = parsedJsonObject.getAsJsonArray("sentences");
         Word verb, noun, noun2, preposition;
 
-        for (JsonElement sentence : sentences) {
-            String rule = sentence.getAsJsonArray().get(sentence.getAsJsonArray().size()-1).getAsJsonObject().get("rule").getAsString();
+        if (sentences == null) {
+            System.out.println("Error in parse!"); //TODO remove sout
+        }
+        else
+        {
+            for (JsonElement sentence : sentences) {
+                String rule = sentence.getAsJsonArray().get(sentence.getAsJsonArray().size() - 1).getAsJsonObject().get("rule").getAsString();
 
-            Map<Integer,Word> words = new HashMap<>();
-            for (int i=0; i<sentence.getAsJsonArray().size()-1; i++) {
-                JsonElement jsonElement = sentence.getAsJsonArray().get(i);
-                words.put(i+1, new Word(jsonElement.getAsJsonObject().get("description").getAsString(),
-                                        jsonElement.getAsJsonObject().get("word").getAsString()) );
-            }
+                Map<Integer, Word> words = new HashMap<>();
+                for (int i = 0; i < sentence.getAsJsonArray().size() - 1; i++) {
+                    JsonElement jsonElement = sentence.getAsJsonArray().get(i);
+                    words.put(i + 1, new Word(jsonElement.getAsJsonObject().get("description").getAsString(),
+                            jsonElement.getAsJsonObject().get("word").getAsString()));
+                }
 
-            switch(rule) {
-                case "V1":                                          // Syntax: Verb
-                    verb = words.get(1);
-                    execute(verb.getDescription());
-                    break;
-                case "V2":                                          // Syntax: Verb, Direction(Noun)
-                    verb = words.get(1);
-                    noun = words.get(2);
-                    execute(verb.getDescription(), noun.getDescription());
-                    break;
-                case "V3":                                          // Syntax: Verb, Item(Noun)
-                    verb = words.get(1);
-                    noun = words.get(2);
-                    execute(verb.getDescription(), noun.getDescription());
-                    break;
-                case "V4":                                          // Syntax: Verb, Item(Noun), Preposition, Item(Noun)
-                    verb = words.get(1);
-                    noun = words.get(2);
-                    preposition = words.get(3);
-                    noun2 = words.get(4);
-                    execute(verb.getDescription(),noun.getDescription(), preposition.getDescription(), noun2.getDescription());
-                    break;
-                case "N3":                                          // Syntax: Noun(command)
-                    noun = words.get(2);
-                    execute(noun.getDescription());
-                    break;
-                default:
-                    break;
+                switch (rule) {
+                    case "V1":                                          // Syntax: Verb
+                        verb = words.get(1);
+                        execute(verb.getDescription());
+                        break;
+                    case "V2":                                          // Syntax: Verb, Direction(Noun)
+                        verb = words.get(1);
+                        noun = words.get(2);
+                        execute(verb.getDescription(), noun.getDescription());
+                        break;
+                    case "V3":                                          // Syntax: Verb, Item(Noun)
+                        verb = words.get(1);
+                        noun = words.get(2);
+                        execute(verb.getDescription(), noun.getDescription());
+                        break;
+                    case "V4":                                          // Syntax: Verb, Item(Noun), Preposition, Item(Noun)
+                        verb = words.get(1);
+                        noun = words.get(2);
+                        preposition = words.get(3);
+                        noun2 = words.get(4);
+                        execute(verb.getDescription(), noun.getDescription(), preposition.getDescription(), noun2.getDescription());
+                        break;
+                    case "N3":                                          // Syntax: Noun(command)
+                        noun = words.get(2);
+                        execute(noun.getDescription());
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
