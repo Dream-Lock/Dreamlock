4465fee17d7f3a3e538cc078d1bf22df11a4f64f
tommy <tommythess1@hotmail.com>
2015-04-27 02:27:03 +0300

    Refactor

--- src/com/dreamlock/parsers/commandParser/constants/Dictionary.java
+++ src/com/dreamlock/parsers/commandParser/constants/Dictionary.java
@@ -11,43 +11,43 @@ public enum Dictionary {
     INSTANCE;
 
     Dictionary() {
-        grammar = new HashMap<>();
+        dictionary = new HashMap<>();
 
         /** VERBS **/
-        grammar.put("look", new Lexeme(1, "look", TokenType.VERB));
-        grammar.put("quit", new Lexeme(1, "quit", TokenType.VERB));
-        grammar.put("exit", new Lexeme(1, "exit", TokenType.VERB));
-        grammar.put("help", new Lexeme(1, "help", TokenType.VERB));
+        dictionary.put("look", new Lexeme(1, "look", TokenType.VERB));
+        dictionary.put("quit", new Lexeme(1, "quit", TokenType.VERB));
+        dictionary.put("exit", new Lexeme(1, "exit", TokenType.VERB));
+        dictionary.put("help", new Lexeme(1, "help", TokenType.VERB));
 
-        grammar.put("go", new Lexeme(2, "go", TokenType.VERB));
-        grammar.put("walk", new Lexeme(2, "walk", TokenType.VERB));
+        dictionary.put("go", new Lexeme(2, "go", TokenType.VERB));
+        dictionary.put("walk", new Lexeme(2, "walk", TokenType.VERB));
 
-        grammar.put("examine", new Lexeme(3, "examine", TokenType.VERB));
-        grammar.put("take", new Lexeme(3, "take", TokenType.VERB));
-        grammar.put("drop", new Lexeme(3, "drop", TokenType.VERB));
+        dictionary.put("examine", new Lexeme(3, "examine", TokenType.VERB));
+        dictionary.put("take", new Lexeme(3, "take", TokenType.VERB));
+        dictionary.put("drop", new Lexeme(3, "drop", TokenType.VERB));
 
-        grammar.put("open", new Lexeme(4, "open", TokenType.VERB));
+        dictionary.put("open", new Lexeme(4, "open", TokenType.VERB));
 
         /** CONJUCTIONS **/
-        grammar.put("and", new Lexeme(1, "and", TokenType.CONJUNCTION));
+        dictionary.put("and", new Lexeme(1, "and", TokenType.CONJUNCTION));
 
         /** PREPOSITIONS **/
-        grammar.put("under", new Lexeme(1, "under", TokenType.PREPOSITION));
-        grammar.put("in", new Lexeme(2, "in", TokenType.PREPOSITION));
-        grammar.put("on", new Lexeme(3, "on", TokenType.PREPOSITION));
+        dictionary.put("under", new Lexeme(1, "under", TokenType.PREPOSITION));
+        dictionary.put("in", new Lexeme(2, "in", TokenType.PREPOSITION));
+        dictionary.put("on", new Lexeme(3, "on", TokenType.PREPOSITION));
 
         /** NOUNS **/
-        grammar.put("west", new Lexeme(1, "west", TokenType.NOUN));
-        grammar.put("north", new Lexeme(1, "north", TokenType.NOUN));
-        grammar.put("east", new Lexeme(1, "east", TokenType.NOUN));
-        grammar.put("south", new Lexeme(1, "south", TokenType.NOUN));
+        dictionary.put("west", new Lexeme(1, "west", TokenType.NOUN));
+        dictionary.put("north", new Lexeme(1, "north", TokenType.NOUN));
+        dictionary.put("east", new Lexeme(1, "east", TokenType.NOUN));
+        dictionary.put("south", new Lexeme(1, "south", TokenType.NOUN));
     }
 
-    private final Map<String, Lexeme> grammar;
+    private final Map<String, Lexeme> dictionary;
 
     public Lexeme getLexeme(String possibleLexeme) {
-        if (grammar.containsKey(possibleLexeme)) {
-            return grammar.get(possibleLexeme);
+        if (dictionary.containsKey(possibleLexeme)) {
+            return dictionary.get(possibleLexeme);
         }
         else {
             return new Lexeme(1, possibleLexeme, TokenType.UNKNOWN);
@@ -55,6 +55,6 @@ public enum Dictionary {
     }
 
     public Set<String> getDefinedGrammarKeys() {
-        return grammar.keySet();
+        return dictionary.keySet();
     }
 }
