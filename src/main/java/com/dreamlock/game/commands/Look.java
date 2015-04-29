355cbe53a118c8a4e855a81f7c09e45771f34727
tommy <tommythess1@hotmail.com>
2015-04-29 07:37:42 +0300

    finish look view

--- src/main/java/com/dreamlock/game/commands/Look.java
+++ src/main/java/com/dreamlock/game/commands/Look.java
@@ -1,20 +1,24 @@
 package com.dreamlock.game.commands;
 
 import com.dreamlock.game.IGameContext;
+import com.dreamlock.game.models.Room;
 import com.dreamlock.game.models.Word;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by Andreas on 29/4/2015.
- */
 public class Look implements ICommand  {
     @Override
     public List<Integer> execute(IGameContext gameContext) {
-        String roomDescription = gameContext.getCurrentRoom().getDescription();
-        System.out.println(roomDescription); //ToDo
-        return null;
+        List<Integer> output = new ArrayList<>();
+        Room room = gameContext.getCurrentRoom();
+
+        output.add(10001);
+        output.add(room.getId());
+        output.add(10003);
+        output.add(10003);
+        return output;
     }
 
     @Override
