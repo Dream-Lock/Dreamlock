c76851b00d160ff96993d10e63c80cf63555115c
Johnny Kall <jkkall7@gmail.com>
2015-05-22 16:44:38 +0300

    Logo adjustment

--- src/main/java/com/dreamlock/Dreamlock.java
+++ src/main/java/com/dreamlock/Dreamlock.java
@@ -231,13 +231,13 @@ public class Dreamlock {
 
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics g = image.getGraphics();
-        g.setFont(new Font("Verdana", Font.BOLD, 14));
+        g.setFont(new Font("Verdana", Font.BOLD, 13));
 
         Graphics2D graphics = (Graphics2D) g;
         graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
                 RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-        graphics.drawString("DREAMLOCK", 2, 10);
+        graphics.drawString("Dreamlock", 2, 10);
 
         for (int y = 0; y < height; y++) {
             StringBuilder sb = new StringBuilder();
