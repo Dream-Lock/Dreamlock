313a33a65236a5dcb4247edf090313573c5cf416
tommy <tommythess1@hotmail.com>
2015-06-03 01:32:28 +0300

    Fix status printing

--- src/main/java/com/dreamlock/core/message_system/StringMessageHandler.java
+++ src/main/java/com/dreamlock/core/message_system/StringMessageHandler.java
@@ -34,53 +34,54 @@ public class StringMessageHandler implements Serializable, IMessageHandler {
     public void print(List<OutputMessage> messageIds) {
 
         for (OutputMessage outputMessage : messageIds) {
+            PrintStyle printStyle = null;
+            IMessage message = messages.get(outputMessage.getId());
             try {
-                PrintStyle printStyle;
+                printStyle = message.getPrintStyle();
+            }
+            catch (Exception e) {
+                printStyle = outputMessage.getPrintStyle();
+            }
 
-                IMessage message = messages.get(outputMessage.getId());
-                if (message.getPrintStyle().equals(PrintStyle.DEFAULT) || outputMessage.getPrintStyle().equals(PrintStyle.NUMBER)) {
-                    printStyle = outputMessage.getPrintStyle();
-                } else {
-                    printStyle = message.getPrintStyle();
-                }
+            if (printStyle.equals(PrintStyle.DEFAULT) || outputMessage.getPrintStyle().equals(PrintStyle.NUMBER)) {
+                printStyle = outputMessage.getPrintStyle();
+            } else {
+                printStyle = message.getPrintStyle();
+            }
 
-                switch (printStyle) {
-                    case ONLY_TITLE:
-                        System.out.println(message.getName());
-                        break;
-                    case ONLY_TITLE_IN_SAME_LINE:
-                        System.out.print(message.getName());
-                        break;
-                    case ONLY_DESCRIPTION:
-                        System.out.println(message.getDescription());
-                        break;
-                    case ONLY_DESCRIPTION_IN_SAME_LINE:
-                        System.out.print(message.getDescription());
-                        break;
-                    case TITLE_DESCRIPTION:
-                        System.out.println(message.getName());
-                        System.out.println(message.getDescription());
-                        break;
-                    case ONLY_EFFECT:
-                        System.out.println(((EffectMessage) message).getEffect());
-                        break;
-                    case BREAK:
-                        System.out.println("");
-                        break;
-                    case NUMBER:
-                        System.out.print(outputMessage.getId());
-                    case EMPTY:
-                        break;
-                    case ARROW:
-                        System.out.print(" --> ");
-                        break;
-                    default:
-                        System.out.println("Print ERROR!!! PLEASE CHANGE DEFAULT PRINTING STYLE!!!");
-                        break;
-                }
-            } catch (Exception e) {
-                System.out.println("This message was not registered: " + outputMessage);
-                e.printStackTrace();
+            switch (printStyle) {
+                case ONLY_TITLE:
+                    System.out.println(message.getName());
+                    break;
+                case ONLY_TITLE_IN_SAME_LINE:
+                    System.out.print(message.getName());
+                    break;
+                case ONLY_DESCRIPTION:
+                    System.out.println(message.getDescription());
+                    break;
+                case ONLY_DESCRIPTION_IN_SAME_LINE:
+                    System.out.print(message.getDescription());
+                    break;
+                case TITLE_DESCRIPTION:
+                    System.out.println(message.getName());
+                    System.out.println(message.getDescription());
+                    break;
+                case ONLY_EFFECT:
+                    System.out.println(((EffectMessage) message).getEffect());
+                    break;
+                case BREAK:
+                    System.out.println("");
+                    break;
+                case NUMBER:
+                    System.out.print(outputMessage.getId());
+                case EMPTY:
+                    break;
+                case ARROW:
+                    System.out.print(" --> ");
+                    break;
+                default:
+                    System.out.println("Print ERROR!!! PLEASE CHANGE DEFAULT PRINTING STYLE!!!");
+                    break;
             }
         }
     }
